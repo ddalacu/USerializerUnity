b5bed784aca99f7d96b41d7692aef15fd0195fa0
Diac Paul <paul.diac@quadom.com>
2022-04-20 18:39:52 +0300

    Updated utils implementation

---
+++ Runtime/SerializationPools.cs
@@ -0,0 +1,39 @@
+﻿using System.IO;
+
+namespace USerialization.Unity
+{
+    public class SerializationPools
+    {
+        public const int PoolSize = 2048 * 4;
+
+        private static readonly ObjectPool<SerializerOutput> OutputPool =
+            new ObjectPool<SerializerOutput>(() => new SerializerOutput(PoolSize), (output) =>
+            {
+                if (output.Stream == null)
+                    return;
+                output.Flush();
+                output.SetStream(null);
+            });
+
+        private static readonly ObjectPool<SerializerInput> InputPool =
+            new ObjectPool<SerializerInput>(() => new SerializerInput(PoolSize), (input) =>
+            {
+                input.FinishRead();
+                input.SetStream(null);
+            });
+
+        public static PooledObjectHandle<SerializerOutput> GetOutput(Stream stream, out SerializerOutput item)
+        {
+            var objectHandle = OutputPool.Get(out item);
+            item.SetStream(stream);
+            return objectHandle;
+        }
+
+        public static PooledObjectHandle<SerializerInput> GetInput(Stream stream, out SerializerInput item)
+        {
+            var objectHandle = InputPool.Get(out item);
+            item.SetStream(stream);
+            return objectHandle;
+        }
+    }
+}
--- Runtime/SerializationUtils.cs
+++ Runtime/SerializationUtils.cs
@@ -1,6 +1,7 @@
 using System;
 using System.IO;
 using System.Reflection;
+using JetBrains.Annotations;
 using UnityEngine;
 
 namespace USerialization.Unity
@@ -8,7 +9,7 @@ namespace USerialization.Unity
     public static class SerializationUtils
     {
         private static USerializer _serializer;
-        
+
         public static USerializer Instance
         {
             get
@@ -28,81 +29,74 @@ namespace USerialization.Unity
                         new ListSerializer()
                     };
 
-                    _serializer = new USerializer(new UnitySerializationPolicy(), providers, new DataTypesDatabase(), logger);
+                    _serializer = new USerializer(new UnitySerializationPolicy(), providers, new DataTypesDatabase(),
+                        logger);
                 }
 
                 return _serializer;
             }
         }
-        
-        public static readonly ObjectPool<SerializerOutput> OutputPool =
-            new ObjectPool<SerializerOutput>(() => new SerializerOutput(2048 * 4), (output) =>
-            {
-                if (output.Stream == null) 
-                    return;
-                output.Flush();
-                output.SetStream(null);
-            });
 
-        public static readonly ObjectPool<SerializerInput> InputPool =
-            new ObjectPool<SerializerInput>(() => new SerializerInput(2048 * 4), (input) =>
-            {
-                input.FinishRead();
-                input.SetStream(null);
-            });
 
         public static void Serialize(this USerializer serializer, Stream stream, object obj)
         {
-            using (OutputPool.Get(out var output))
+            using (SerializationPools.GetOutput(stream, out var output))
             {
-                output.SetStream(stream);
-                
-                var success = serializer.Serialize(output, obj);
-                if (success == false)
-                    throw new Exception($"Failed to serialize {obj}, returning default!");
+                Serialize(serializer, obj, output);
             }
         }
 
-        public static void PopulateObject<T>(this USerializer serializer, Stream stream, ref T data)
+        public static void Serialize(this USerializer serializer, [NotNull] object obj, SerializerOutput output)
         {
-            using (InputPool.Get(out var input))
+            if (serializer.TryGetClassHelper(out var helper, obj.GetType()) == false)
+                throw new Exception($"Failed to get serializer for {obj}, returning default!");
+
+            helper.SerializeObject(obj, output, null);
+        }
+
+        public static void Populate<T>(this USerializer serializer, Stream stream, ref T data) where T : class
+        {
+            using (SerializationPools.GetInput(stream, out var input))
             {
-                input.SetStream(stream);
-                
-                var success = serializer.TryDeserialize(input, ref data);
-                if (success == false)
-                    throw new Exception($"Failed to serialize {typeof(T)}, returning default!");
+                Populate(serializer, ref data, input);
             }
         }
 
-        public static void PopulateObject<T>(this USerializer serializer, Stream stream, T data) where T : class
+        public static void Populate<T>(this USerializer serializer, ref T data, SerializerInput input)
+            where T : class
         {
-            if (data == null)
-                throw new Exception("LoadFromStream called with null object!");
+            ClassSerializationHelper helper;
 
-            using (InputPool.Get(out var input))
+            if (data != null)
             {
-                input.SetStream(stream);
-                
-                var success = serializer.TryDeserialize(input, ref data);
+                if (serializer.TryGetClassHelper(out helper, data.GetType()) == false)
+                    throw new Exception($"Failed to get serializer for {data}, returning default!");
 
-                if (success == false)
-                    throw new Exception($"Failed to serialize {typeof(T)}, returning default!");
+                helper.PopulateObject(data, input, null);
+            }
+            else
+            {
+                if (serializer.TryGetClassHelper(out helper, typeof(T)) == false)
+                    throw new Exception($"Failed to get serializer for {data}, returning default!");
+
+                data = (T) helper.DeserializeObject(input, null);
             }
         }
 
-        public static T Deserialize<T>(this USerializer serializer, Stream stream)
+        public static T Deserialize<T>(this USerializer serializer, Stream stream) where T : class
         {
-            using (InputPool.Get(out var input))
+            using (SerializationPools.GetInput(stream, out var input))
             {
-                input.SetStream(stream);
-                T data = default;
-                var success = serializer.TryDeserialize(input, ref data);
-
-                if (success == false)
-                    throw new Exception($"Failed to serialize {typeof(T)}, returning default!");
-                return data;
+                return Deserialize<T>(serializer, input);
             }
+        }
+
+        public static T Deserialize<T>(this USerializer serializer, SerializerInput input) where T : class
+        {
+            if (serializer.TryGetClassHelper(out var helper, typeof(T)) == false)
+                throw new Exception($"Failed to get serializer for {typeof(T)}, returning default!");
+
+            return (T) helper.DeserializeObject(input, null);
         }
     }
 }
